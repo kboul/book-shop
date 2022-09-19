ff01ab67f3dd3501416e9b8862daff2258ad5e23
kboul <kos.voulgaridis@gmail.com>
2022-09-19 15:40:20 +0300

    Configure & create basic book routes and init their components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { AddBook, Books, UpdateBook } from "./pages";
+
 export default function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<Books />} />
+        <Route path="/add-book" element={<AddBook />} />
+        <Route path="/update-book" element={<UpdateBook />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
